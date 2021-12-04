dcde3f5c86d3dc4c71777d3d858de836e634ac6b
Thành Nhân <nhangaming125@gmail.com>
2021-12-04 16:45:38 +0700

    PM4

--- README.md
+++ README.md
@@ -1,25 +1,72 @@
 # General
-**Keep player's inventory after player death**
+**Keep items in the player's inventory after the player dies**
+
+[![](https://poggit.pmmp.io/ci.shield/nhanaz-pm-pl/KeepInventory/KeepInventory)](https://poggit.pmmp.io/ci/nhanaz-pm-pl/KeepInventory/KeepInventory)
+[![](https://poggit.pmmp.io/shield.state/KeepInventory)](https://poggit.pmmp.io/p/KeepInventory)
+[![](https://poggit.pmmp.io/shield.api/KeepInventory)](https://poggit.pmmp.io/p/KeepInventory)
+[![](https://poggit.pmmp.io/shield.dl.total/KeepInventory)](https://poggit.pmmp.io/p/KeepInventory)
+[![](https://poggit.pmmp.io/shield.dl/KeepInventory)](https://poggit.pmmp.io/p/KeepInventory)
 
 # Config
 ```yaml
-# Config file
-# Keep inventory true or false
+---
+# KeepInventory
+#
+# Value: true or false
+# Default value: true (Recomend using)
+#
+# Value description:
+# KeepInventory: true (Items in the player's inventory will be kept after the they die)
+# KeepInventory: false (Items in the player's inventory will be lost after the they die)
 KeepInventory: true
 
-# Message types: message, popup, tip, title
-MessageType: message
 
-# Message after player's inventory has been kept
-MessageAfterDeath: "§aYour inventory has been kept"
+# MessageAfterDeath
+#
+# Value: "MessageAfterDeath (String)"
+# Default value: §aItems in your inventory have been kept
+#
+# Value description: Notifications sent to players after they die
+MessageAfterDeath: "Items in your inventory have been kept"
+
+# Message types
+#
+# Value: none, message, popup, tip, title, subtitle, actionbarmessage
+# Default value: none (Recomend using)
+#
+# Value description:
+# MessageType: none (No messages have been sent to the player)
+# MessageType: message (Sends a direct chat message to a player)
+# MessageType: title (Send a title text to the user's screen)
+# MessageType: popup (Sends a popup message to the player)
+# MessageType: tip (Sends a tip message to the player)
+# MessageType: actionbarmessage (Send a small text to the user's screen)
+MessageType: none
+
+# Notices
+#
+# Value: true or false
+# Default value: true (Recomend using)
+#
+# Value description:
+# Notices: true (Notifications related to this plugin will be displayed on the console)
+# Notices: false (Notifications related to this plugin will not be displayed on the console)
+Notices: true
+...
+
+
 ```
 
 # Contacts
 **You can contact me directly through the platforms listed below**
-| Platform | KhoaGamingVN            | NhanAZ       |
-| -------- | ----------------------- | ------------ |
-| Discord  | Dangkhoa2683#6985       | NhanAZ#9115  |
-| Email    | khoagaming123@gmail.com | NhanAZ@pm.me |
+| Platform | Contact            |
+| -------- | ------------------ |
+| Discord  | NhanAZ#9115        |
+| Email    | NhanAZ@pm.me       |
+| Twitter  | @ThanhNhanAZ       |
+| Zalo     | @ThanhNhanAZ       |
+| FaceBook | fb.com/ThanhNhanAZ |
+
 
 # License
 [**GNU General Public License v3.0**](https://www.gnu.org/licenses/gpl-3.0.html)
--- src/Main.php
+++ src/Main.php
@@ -1,26 +1,60 @@
 <?php
 
-namespace KhoaGamingPro\KeepInventory;
+/**
+ * Growth plugin for PocketMine-MP
+ * Copyright (C) 2021 NhanAZ <https://github.com/NhanAZ>
+ *
+ * Growth is licensed under the GNU General Public License v3.0 (GPL-3.0 License)
+ *
+ * GNU General Public License <https://www.gnu.org/licenses/>
+ *
+ * Discord :: NhanAZ#9115
+ * Email   :: NhanAZ@pm.me
+ * Twitter :: @ThanhNhanAZ
+ */
+
+declare(strict_types=1);
+
+namespace NhanAZ\KeepInventory;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDeathEvent;
 
+/**
+ * Class Main
+ * @package NhanAZ\KeepInventory
+ */
 class Main extends PluginBase implements Listener
 {
 
-
-	public function onEnable() : void
+	/**
+	 * @return void
+	 */
+	protected function onEnable() : void
 	{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
-		if ($this->getConfig()->get("KeepInventory") == true) {
-			$this->getServer()->getLogger()->notice("Keep inventory enabled. Player's inventory will keep after death. You can disable it in config.yml");
+		$notices = $this->getConfig()->get("Notices");
+		$keepInventory = $this->getConfig()->get("KeepInventory");
+		if ($notices == true) {
+			if ($keepInventory == true) {
+				$this->getLogger()->notice("Items in the player's inventory that will be kept after they die are enabled.");
+				$this->getLogger()->notice("Edit `KeepInventory: true` to `KeepInventory: false` in `config.yml` to disable.");
+			} else {
+				$this->getLogger()->notice("Items in the player's inventory that will not be kept after they die are enabled.");
+				$this->getLogger()->notice("Edit `KeepInventory: false` to `KeepInventory: true` in `config.yml` to enable.");
+			}
 		} else {
-			$this->getServer()->getLogger()->notice("Keep inventory disabled. Player's inventory won't keep after death. You can enable it in config.yml");
+			$this->getLogger()->warning("The notifications related to this plugin are being disabled!");
+			$this->getLogger()->warning("Edit `Notices: true` in `config.yml` to enable!");
 		}
 	}
 
+	/**
+	 * @param PlayerDeath $event
+	 * @priority HIGHEST
+	 */
 	public function PlayerDeath(PlayerDeathEvent $event)
 	{
 		$player = $event->getPlayer();
@@ -29,17 +63,22 @@ class Main extends PluginBase implements Listener
 		$messageAfterDeath = $this->getConfig()->get("MessageAfterDeath");
 		if ($keepInventory == true) {
 			$event->setKeepInventory(true);
-			if ($messageType == "message") {
-				$player->sendMessage($messageAfterDeath);
-			}
-			if ($messageType == "popup") {
-				$player->sendPopup($messageAfterDeath);
-			}
-			if ($messageType == "tip") {
-				$player->sendTip($messageAfterDeath);
-			}
-			if ($messageType == "title") {
-				$player->sendTitle($messageAfterDeath);
+			if ($messageType !== "none") {
+				if ($messageType == "message") {
+					$player->sendMessage($messageAfterDeath);
+				}
+				if ($messageType == "title") {
+					$player->sendTitle($messageAfterDeath);
+				}
+				if ($messageType == "popup") {
+					$player->sendPopup($messageAfterDeath);
+				}
+				if ($messageType == "tip") {
+					$player->sendTip($messageAfterDeath);
+				}
+				if ($messageType == "actionbarmessage") {
+					$player->sendActionBarMessage($messageAfterDeath);
+				}
 			}
 		} else {
 			$event->setKeepInventory(false);
