007e22d15e6f4562e6f5df5d285faa7c2133d95d
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-06-03 08:34:58 +0700

    Just some changes!

--- src/KeepInventory.php
+++ src/KeepInventory.php
@@ -2,24 +2,28 @@
 
 declare(strict_types=1);
 
-namespace NhanAZ\KeepInventory;
+namespace KeepInventory;
 
 use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use function in_array;
 
-class Main extends PluginBase implements Listener {
+class KeepInventory extends PluginBase implements Listener {
+	protected Config $config;
 
-	protected function onEnable(): void {
+	protected function onEnable() : void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
+		$this->config = $this->getConfig();
 	}
 
 	public function keepInventory($event) {
 		$player = $event->getPlayer();
 		$event->setKeepInventory(true);
-		$msgAfterDeath = $this->getConfig()->get("MsgAfterDeath", "You died, but your inventory is safe!");
-		match ($this->getConfig()->get("MsgType", "none")) {
+		$msgAfterDeath = $this->config->get("MsgAfterDeath", "You died, but your inventory is safe!");
+		match ($this->config->get("MsgType", "none")) {
 			"message" => $player->sendMessage($msgAfterDeath),
 			"title" => $player->sendTitle($msgAfterDeath),
 			"popup" => $player->sendPopup($msgAfterDeath),
@@ -30,20 +34,20 @@ class Main extends PluginBase implements Listener {
 	}
 
 	public function onPlayerDeath(PlayerDeathEvent $event) {
-		if ($this->getConfig()->get("KeepInventory", true)) {
+		if ($this->config->get("KeepInventory", true)) {
 			$worldName = $event->getPlayer()->getWorld()->getDisplayName();
-			$worlds = $this->getConfig()->get("Worlds", []);
-			switch ($this->getConfig()->get("Mode", "all")) {
+			$worlds = $this->config->get("Worlds", []);
+			switch ($this->config->get("Mode", "all")) {
 				case "all":
 					$this->keepInventory($event);
 					break;
 				case "whitelist":
-					if (in_array($worldName, $worlds)) {
+					if (in_array($worldName, $worlds, true)) {
 						$this->keepInventory($event);
 					}
 					break;
 				case "blacklist":
-					if (!in_array($worldName, $worlds)) {
+					if (!in_array($worldName, $worlds, true)) {
 						$this->keepInventory($event);
 					}
 					break;
