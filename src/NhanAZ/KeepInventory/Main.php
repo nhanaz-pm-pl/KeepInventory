ec115877db5dae7528532f168aa925d000cd6d38
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-08-29 14:19:48 +0700

    configVersion

--- src/NhanAZ/KeepInventory/Main.php
+++ src/NhanAZ/KeepInventory/Main.php
@@ -11,7 +11,21 @@ use pocketmine\event\player\PlayerDeathEvent;
 
 class Main extends PluginBase {
 
+	const CONFIG_VERSION = "1.0.0";
+
+	private function checkConfig(): void {
+		$configVersion = $this->getConfig()->exists("ConfigVersion") ? $this->getConfig()->get("ConfigVersion") : "0.0.0";
+		if (version_compare($configVersion, self::CONFIG_VERSION, "<>")) {
+			$this->getLogger()->warning("Your configuration file is invalid, updating the config.yml...");
+			$this->getLogger()->warning("Invalid configuration file can be found at config_invalid.yml");
+			rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_invalid.yml");
+			$this->saveDefaultConfig();
+			$this->getConfig()->reload();
+		}
+	}
+
 	protected function onEnable(): void {
+		$this->checkConfig();
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 		$this->saveDefaultConfig();
 	}
