32d11a1d6de52524c43d94ccfe6a2a8a5b286357
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-05-18 11:39:13 +0700

    Add EventPriority

--- src/NhanAZ/KeepInventory/Main.php
+++ src/NhanAZ/KeepInventory/Main.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace NhanAZ\KeepInventory;
 
+use pocketmine\event\EventPriority;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDeathEvent;
@@ -11,27 +12,37 @@ use pocketmine\event\player\PlayerDeathEvent;
 class Main extends PluginBase implements Listener {
 
 	protected function onEnable(): void {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$pluginMgr = $this->getServer()->getPluginManager();
+		$pluginMgr->registerEvents($this, $this);
 		$this->saveDefaultConfig();
+		try {
+			$onDeath = \Closure::fromCallable([$this, "onPlayerDeath"]);
+			$pluginMgr->registerEvent(PlayerDeathEvent::class, $onDeath, EventPriority::HIGHEST, $this);
+		} catch (\ReflectionException $e) {
+			$this->getLogger()->critical($e->getMessage());
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+		}
 	}
 
 	public function onPlayerDeath(PlayerDeathEvent $event): void {
-		if ($this->getConfig()->get("keepInventory")) {
-			$worldName = $event->getPlayer()->getWorld()->getDisplayName();
-			$worlds = $this->getConfig()->get("worlds");
-			$isBlacklist = match (strval($this->getConfig()->get("mode"))) {
-				"blacklist" => true,
-				"whitelist" => false,
-				default => true
-			};
-			if ($isBlacklist) {
-				if (!in_array($worldName, $worlds)) {
-					$event->setKeepInventory(true);
-				}
+		$worldName = $event->getPlayer()->getWorld()->getDisplayName();
+		$worlds = $this->getConfig()->get("worlds");
+		$isBlacklist = match (strval($this->getConfig()->get("mode"))) {
+			"blacklist" => true,
+			"whitelist" => false,
+			default => true
+		};
+		if ($isBlacklist) {
+			if (!in_array($worldName, $worlds)) {
+				$event->setKeepInventory(true);
 			} else {
-				if (in_array($worldName, $worlds)) {
-					$event->setKeepInventory(false);
-				}
+				$event->setKeepInventory(false);
+			}
+		} else {
+			if (in_array($worldName, $worlds)) {
+				$event->setKeepInventory(true);
+			} else {
+				$event->setKeepInventory(false);
 			}
 		}
 	}
