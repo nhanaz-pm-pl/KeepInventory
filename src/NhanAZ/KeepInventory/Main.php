f6ef022797e89e9be20dffb5558bfca9b1662e9e
Taylor.R <nguyenhieucoder47@gmail.com>
2023-06-01 17:56:20 +0700

    Clean Code

--- src/NhanAZ/KeepInventory/Main.php
+++ src/NhanAZ/KeepInventory/Main.php
@@ -9,41 +9,44 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDeathEvent;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
+    protected function onEnable(): void
+    {
+        $pluginMgr = $this->getServer()->getPluginManager();
+        $pluginMgr->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        try {
+            $onDeath = $this->onPlayerDeath(...);
+            $pluginMgr->registerEvent(
+                PlayerDeathEvent::class,
+                $onDeath,
+                EventPriority::HIGHEST,
+                $this
+            );
+        } catch (\ReflectionException $e) {
+            $this->getLogger()->critical($e->getMessage());
+            $this->getServer()
+                ->getPluginManager()
+                ->disablePlugin($this);
+        }
+    }
 
-	protected function onEnable(): void {
-		$pluginMgr = $this->getServer()->getPluginManager();
-		$pluginMgr->registerEvents($this, $this);
-		$this->saveDefaultConfig();
-		try {
-			$onDeath = \Closure::fromCallable([$this, "onPlayerDeath"]);
-			$pluginMgr->registerEvent(PlayerDeathEvent::class, $onDeath, EventPriority::HIGHEST, $this);
-		} catch (\ReflectionException $e) {
-			$this->getLogger()->critical($e->getMessage());
-			$this->getServer()->getPluginManager()->disablePlugin($this);
-		}
-	}
-
-	public function onPlayerDeath(PlayerDeathEvent $event): void {
-		$worldName = $event->getPlayer()->getWorld()->getDisplayName();
-		$worlds = $this->getConfig()->get("worlds");
-		$isBlacklist = match (strval($this->getConfig()->get("mode"))) {
-			"blacklist" => true,
-			"whitelist" => false,
-			default => true
-		};
-		if ($isBlacklist) {
-			if (!in_array($worldName, $worlds)) {
-				$event->setKeepInventory(true);
-			} else {
-				$event->setKeepInventory(false);
-			}
-		} else {
-			if (in_array($worldName, $worlds)) {
-				$event->setKeepInventory(true);
-			} else {
-				$event->setKeepInventory(false);
-			}
-		}
-	}
+    public function onPlayerDeath(PlayerDeathEvent $event): void
+    {
+        $worldName = $event
+            ->getPlayer()
+            ->getWorld()
+            ->getDisplayName();
+        $worlds = $this->getConfig()->get("worlds");
+        $isBlacklist = match (strval($this->getConfig()->get("mode"))) {
+            "blacklist" => true,
+            "whitelist" => false,
+            default => true,
+        };
+        $shouldKeepInventory =
+            ($isBlacklist && !in_array($worldName, $worlds)) ||
+            (!$isBlacklist && in_array($worldName, $worlds));
+        $event->setKeepInventory($shouldKeepInventory);
+    }
 }
